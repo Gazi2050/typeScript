3c6852297e176b31b889c7a692a35d2caddeed4d
Gazi2050 <gazi45344@gmail.com>
2024-01-23 22:38:59 +0600

    day 2

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 var user = 'jack';
+var age = 20;
+console.log(age);
 console.log(user);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+//built-in type : number,string,boolean,void,undefined,null
+var num;
+num = 56;
+console.log(num);
+var Name;
+Name = 'araf';
+console.log(Name);
+var bul;
+bul = true;
+console.log(bul);
+var first = 'black ';
+var last = 'hole';
+var full = first.concat(last);
+console.log(full);
+//void
+function display() {
+    console.log('infinity void');
+}
+display();
--- index.ts
+++ index.ts
@@ -1,2 +1,29 @@
 let user = 'jack';
+let age = 20;
+console.log(age);
 console.log(user);
+
+function addNumbers(num1: number, num2: number) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+
+//built-in type : number,string,boolean,void,undefined,null
+let num: number;
+num = 56;
+console.log(num);
+let Name: string;
+Name = 'araf';
+console.log(Name);
+let bul: boolean;
+bul = true;
+console.log(bul);
+let first = 'black '
+let last = 'hole'
+let full = first.concat(last);
+console.log(full);
+//void
+function display(): void {
+    console.log('infinity void');
+}
+display();
