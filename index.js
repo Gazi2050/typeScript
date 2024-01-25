0049f88d25769b916835f75aecb2ea38f6122f2d
Gazi2050 <gazi45344@gmail.com>
2024-01-26 01:22:40 +0600

    enum type

--- index.js
+++ index.js
@@ -1,27 +1,100 @@
 var user = 'jack';
 var age = 20;
-console.log(age);
-console.log(user);
+//console.log(age);
+//console.log(user);
 function addNumbers(num1, num2) {
-    console.log(num1 + num2);
+    //console.log(num1 + num2);
 }
 addNumbers(20, 30);
-//built-in type : number,string,boolean,void,undefined,null
+//built-in type : number,string,boolean,void
 var num;
 num = 56;
-console.log(num);
+//console.log(num);
 var Name;
 Name = 'araf';
-console.log(Name);
+//console.log(Name);
 var bul;
 bul = true;
-console.log(bul);
+//console.log(bul);
 var first = 'black ';
 var last = 'hole';
 var full = first.concat(last);
-console.log(full);
+//console.log(full);
 //void
 function display() {
-    console.log('infinity void');
+    //console.log('infinity void');
 }
 display();
+//any type
+var v = 1;
+v = "v";
+//console.log(v);
+//unknown type
+var w = 1;
+w = "w";
+//console.log(w);
+//union type
+var a;
+a = 1;
+a = 'a';
+//console.log(a);
+function Display(b) {
+    //console.log(b);
+}
+Display('infinity string');
+Display(9);
+Display(true);
+/***********************array******************************/
+var A;
+A = ['a', 'b', 'c', 'd'];
+//console.log(A);
+//console.log(A[3]);
+var B;
+B = [7, 6, 4, 3];
+//console.log(B);
+//console.log(B[2]);
+var C;
+C = ['C', 'b', 'c', true, 'd', 8, 9, false, 65];
+C.sort();
+//console.log(C);
+//console.log(C[5]);
+//tuple type
+var ab;
+ab = [77, 'ab'];
+// console.log(ab);
+//enum type
+/*enum type: 1)numeric
+             2)string
+             3)heterogenous
+*/
+// 1) numeric enum
+var requestType;
+(function (requestType) {
+    requestType[requestType["data"] = 0] = "data";
+    requestType[requestType["number"] = 1] = "number";
+    requestType[requestType["post"] = 2] = "post";
+    requestType[requestType["delete"] = 3] = "delete";
+})(requestType || (requestType = {}));
+// console.log(requestType);
+// 2) string
+var requestType2;
+(function (requestType2) {
+    requestType2["data"] = "q";
+    requestType2["number"] = "o";
+    requestType2["post"] = "l";
+    requestType2["delete"] = "j";
+})(requestType2 || (requestType2 = {}));
+// console.log(requestType2);
+// console.log(requestType2.data);
+// console.log(requestType2['data']);
+// 3) heterogenous
+var requestType3;
+(function (requestType3) {
+    requestType3["data"] = "q";
+    requestType3["number"] = "o";
+    requestType3["post"] = "l";
+    requestType3["delete"] = "j";
+    requestType3[requestType3["id"] = 898] = "id";
+    requestType3[requestType3["userId"] = 876] = "userId";
+})(requestType3 || (requestType3 = {}));
+console.log(requestType3);
--- index.ts
+++ index.ts
@@ -1,29 +1,107 @@
 let user = 'jack';
 let age = 20;
-console.log(age);
-console.log(user);
+//console.log(age);
+//console.log(user);
 
 function addNumbers(num1: number, num2: number) {
-    console.log(num1 + num2);
+    //console.log(num1 + num2);
 }
 addNumbers(20, 30);
 
-//built-in type : number,string,boolean,void,undefined,null
+//built-in type : number,string,boolean,void
 let num: number;
 num = 56;
-console.log(num);
+//console.log(num);
 let Name: string;
 Name = 'araf';
-console.log(Name);
+//console.log(Name);
 let bul: boolean;
 bul = true;
-console.log(bul);
+//console.log(bul);
 let first = 'black '
 let last = 'hole'
 let full = first.concat(last);
-console.log(full);
+//console.log(full);
 //void
 function display(): void {
-    console.log('infinity void');
+    //console.log('infinity void');
 }
 display();
+//any type
+let v: any = 1;
+v = "v";
+//console.log(v);
+//unknown type
+let w: unknown = 1;
+w = "w";
+//console.log(w);
+//union type
+let a: string | number;
+a = 1;
+a = 'a';
+//console.log(a);
+function Display(b: string | number | boolean) {
+    //console.log(b);
+}
+Display('infinity string');
+Display(9);
+Display(true);
+
+/***********************array******************************/
+
+let A: string[];
+A = ['a', 'b', 'c', 'd']
+//console.log(A);
+//console.log(A[3]);
+
+let B: Array<Number>;
+B = [7, 6, 4, 3]
+//console.log(B);
+//console.log(B[2]);
+
+let C: (string | number | boolean)[];
+C = ['C', 'b', 'c', true, 'd', 8, 9, false, 65]
+C.sort()
+//console.log(C);
+//console.log(C[5]);
+
+//tuple type
+let ab: [number, string]
+ab = [77, 'ab']
+// console.log(ab);
+
+//enum type
+/*enum type: 1)numeric
+             2)string
+             3)heterogenous
+*/
+// 1) numeric enum
+enum requestType {
+    data,
+    number,
+    post,
+    delete
+}
+// console.log(requestType);
+
+// 2) string
+enum requestType2 {
+    data = 'q',
+    number = 'o',
+    post = 'l',
+    delete = 'j'
+}
+// console.log(requestType2);
+// console.log(requestType2.data);
+// console.log(requestType2['data']);
+
+// 3) heterogenous
+enum requestType3 {
+    data = 'q',
+    number = 'o',
+    post = 'l',
+    delete = 'j',
+    id = 898,
+    userId = 876
+}
+console.log(requestType3);
